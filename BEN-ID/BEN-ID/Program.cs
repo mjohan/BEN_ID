40f33627618ab5730b754c19277d1b1f91fecffa
Muhammad Johan Alibasa <m.johan.alibasa@gmail.com>
2021-06-17 17:35:51 +1000

    change message

--- BEN-ID/BEN-ID/Program.cs
+++ BEN-ID/BEN-ID/Program.cs
@@ -6,7 +6,7 @@ namespace BEN_ID
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
         }
     }
 }
